5503962f10931983ef0f166954dfb7302b35bdc2
Kisoty <kisoty@gmail.com>
2021-03-03 00:19:01 +0300

    homepage

---
+++ src/Controller/ConferenceController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ConferenceController extends AbstractController
+{
+    /**
+     * @Route("/hello/{name}", name="homepage")
+     */
+    public function index(string $name = ''): Response
+    {
+        return $this->render('conference/index.html.twig', [
+            'controller_name' => 'ConferenceController',
+            'name' => htmlspecialchars($name)
+        ]);
+    }
+}
