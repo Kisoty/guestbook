99ee493a23f98d97509a66ada8af8c528142f257
Kisoty <kisoty@gmail.com>
2021-03-03 23:53:36 +0300

    easy admin init

---
+++ src/Controller/Admin/ConferenceCrudController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Conference;
+use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\BooleanField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+
+class ConferenceCrudController extends AbstractCrudController
+{
+    public static function getEntityFqcn(): string
+    {
+        return Conference::class;
+    }
+
+
+    public function configureFields(string $pageName): iterable
+    {
+        return [
+            TextField::new('year'),
+            TextField::new('city'),
+            BooleanField::new('isInternational')
+        ];
+    }
+
+}
