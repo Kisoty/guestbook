4754cb4ae6b74053c2fa4133793685ad84775474
Kisoty <kisoty@gmail.com>
2021-03-22 14:00:44 +0300

    added feedback forms, photos

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class CommentCrudController extends AbstractCrudController
@@ -28,7 +29,7 @@ class CommentCrudController extends AbstractCrudController
             TextField::new('text'),
             EmailField::new('email'),
             DateField::new('createdAt')->onlyOnIndex(),
-            TextField::new('photoFilename')
+            ImageField::new('photoFilename', 'Photo')->setBasePath('uploads/photos')
         ];
     }
 
--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Conference;
+use App\Form\CommentFormType;
 use App\Repository\CommentRepository;
-use App\Repository\ConferenceRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\File\Exception\FileException;
+use Symfony\Component\HttpFoundation\File\File;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ConferenceController extends AbstractController
 {
+    private $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
     /**
      * @Route("/", name="homepage")
      */
@@ -23,8 +34,35 @@ class ConferenceController extends AbstractController
     /**
      * @Route("/conference/{slug}", name="conferenceDetails")
      */
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(Request $request, Conference $conference, CommentRepository $commentRepository, string $photoDir): Response
     {
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+
+            /** @var File $photo */
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)) . '.' . $photo->guessExtension();
+
+                try {
+                    $photo->move($photoDir, $filename);
+                } catch (FileException $e) {
+                    // can't save photo, give up
+                }
+                $comment->setPhotoFilename($filename);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conferenceDetails', [
+                'slug' => $conference->getSlug()
+            ]);
+        }
+
         $offset = max(0, $request->query->getInt('offset'));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
 
@@ -32,7 +70,8 @@ class ConferenceController extends AbstractController
             'conference' => $conference,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::DEFAULT_ITEMS_PER_PAGE,
-            'next' => min(count($paginator), $offset + CommentRepository::DEFAULT_ITEMS_PER_PAGE)
+            'next' => min(count($paginator), $offset + CommentRepository::DEFAULT_ITEMS_PER_PAGE),
+            'commentForm' => $form->createView()
         ]);
     }
 }
